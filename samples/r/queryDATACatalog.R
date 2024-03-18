b5005587b9d4aa66bf381f3fade067d0457de45c
Arturo <C.A.Gonzalez@cgiar.org>
2024-03-18 15:50:12 -0500

    Updating samples

---
+++ samples/r/queryDATACatalog.R
@@ -0,0 +1,12 @@
+library('rstac')
+collectionUrl = "https://raw.githubusercontent.com/alliance-datascience/datacube-catalog/main/catalog/chirps/collection.json"
+catalog <- rstac::read_stac(collectionUrl)
+getItemsSample <- rstac::read_items(catalog)
+metadata <- rstac::items_select(getItemsSample)%>%rstac::assets_select()
+
+
+
+   
+
+ 
+ 
